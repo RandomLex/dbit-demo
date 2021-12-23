f0c010cc3b778e92079b4918a6557f02775bf6b8
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-12-23 21:05:44 +0300

    18-spring-mvc-java-based-modern

--- app/src/main/java/com/dbit/app/controllers/DepartmentJsonController.java
+++ app/src/main/java/com/dbit/app/controllers/DepartmentJsonController.java
@@ -1,37 +1,31 @@
 package com.dbit.app.controllers;
 
 import com.dbit.app.repositories.DepartmentRepository;
-import com.dbit.app.repositories.EmployeeRepository;
 import com.dbit.model.Department;
-import com.dbit.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import static org.springframework.util.StringUtils.capitalize;
 
-//@WebServlet("/api/departments")
 @Controller
+@RequestMapping(path = "/api/departments", produces = "application/json")
+@ResponseBody
 public class DepartmentJsonController {
     static final String DEPARTMENT_REPOSITORY_PREFIX = "departmentRepository";
     private static final String ID = "id";
@@ -50,14 +44,15 @@ public class DepartmentJsonController {
         repository = repositoryMap.get(DEPARTMENT_REPOSITORY_PREFIX + capitalize(repositoryType));
     }
 
-    @RequestMapping(path = "/api/departments", produces = "application/json", method = RequestMethod.GET)
-    @ResponseBody
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public List<Department> getAll() {
+
         return repository.findAll();
     }
 
-    @RequestMapping(path = "/api/departments/{id}", produces = "application/json", method = RequestMethod.GET)
-    @ResponseBody
+//    @RequestMapping(path = "/api/departments/{id}", method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public Department getDepartment(@PathVariable int id) {
         Optional<Department> department = repository.find(id);
         if (department.isEmpty()) {
@@ -66,23 +61,27 @@ public class DepartmentJsonController {
         return department.get();
     }
 
-    @RequestMapping(path = "/api/departments", produces = "application/json", method = RequestMethod.POST)
-    @ResponseBody
+//    @RequestMapping( method = RequestMethod.POST)
+    @PostMapping
     public Department createDepartment(@RequestBody Department department) {
         return repository.save(department);
     }
 
-    @RequestMapping(path = "/api/departments", produces = "application/json", method = RequestMethod.PUT)
-    @ResponseBody
-    public Department updateDepartment(@RequestBody Department department) {
+//    @RequestMapping(method = RequestMethod.PUT)
+    @PutMapping("/{id}")
+    public Department updateDepartment(
+            @PathVariable int id,
+            @RequestBody Department department) {
+        if (department != null && id != department.getId()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Department id must be equal with id in path: " + id + " != " + department.getId());
+        }
         return repository.save(department);
     }
 
-
-
-    @RequestMapping(path = "/api/departments/{id}", produces = "application/json", method = RequestMethod.DELETE)
-    @ResponseBody
-    protected Department deleteDepartment(@PathVariable int id) {
+//    @RequestMapping(path = "/api/departments/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
+    public Department deleteDepartment(@PathVariable int id) {
         Optional<Department> department = repository.find(id);
         if (department.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Department with id: " + id);
