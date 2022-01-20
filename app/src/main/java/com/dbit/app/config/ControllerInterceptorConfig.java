ff1965e472efc415c05159fc1f67c1c90ad36f27
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-01-20 21:33:43 +0300

    32-spring-transaction-open-entity-manager-in-view

--- app/src/main/java/com/dbit/app/config/ControllerInterceptorConfig.java
+++ app/src/main/java/com/dbit/app/config/ControllerInterceptorConfig.java
@@ -2,7 +2,10 @@ package com.dbit.app.config;
 
 import com.dbit.app.controllers.interceptors.LoggingInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,9 +13,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class ControllerInterceptorConfig implements WebMvcConfigurer {
     private final LoggingInterceptor loggingInterceptor;
+    private final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addWebRequestInterceptor(openEntityManagerInViewInterceptor());
         registry.addInterceptor(loggingInterceptor);
+    }
+
+    @Bean
+    public OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor() {
+        OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor = new OpenEntityManagerInViewInterceptor();
+        openEntityManagerInViewInterceptor.setEntityManagerFactory(entityManagerFactoryBean.getObject());
+        return openEntityManagerInViewInterceptor;
     }
 }
--- app/src/main/java/com/dbit/app/services/AbstractService.java
+++ app/src/main/java/com/dbit/app/services/AbstractService.java
@@ -4,6 +4,7 @@ import com.dbit.app.repositories.Repository;
 import com.dbit.model.dbit.AbstractEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -11,27 +12,31 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+//@Transactional
 public abstract class AbstractService<T extends AbstractEntity> implements ServiceInterface<T> {
 
     protected final Repository<T> repository;
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
     public List<T> getAll() {
         return repository.findAll();
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
     public Optional<T> get(int id) {
         return repository.find(id);
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public T save(T entity) {
         return repository.save(entity);
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public Optional<T> delete(T entity) {
         return repository.remove(entity);
     }
